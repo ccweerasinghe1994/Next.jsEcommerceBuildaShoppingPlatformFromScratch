993d8c0c019334276d7072db3de2ca14a06f9b06
wchamara <chamara.weerasinghe@my.sliit.lk>
2025-04-09 18:51:16 +0530

    shopping cart page initial commit

--- ngnexusstore/app/(root)/cart/cart-table.tsx
+++ ngnexusstore/app/(root)/cart/cart-table.tsx
@@ -1,6 +1,32 @@
-import React from "react";
+"use client";
 
+import { useToast } from "@/hooks/use-toast";
+import { TCart } from "@/types";
+import Link from "next/link";
+import { useRouter } from "next/navigation";
+import { useTransition } from "react";
 
-export default function CartTable() {
-  return <div>CartTable</div>;
+type CartTableProps = {
+  cart?: TCart;
+};
+
+export default function CartTable({ cart }: CartTableProps) {
+  const router = useRouter();
+  const { toast } = useToast();
+  const [isPending, startTransition] = useTransition();
+
+  return (
+    <>
+      <h2 className="py-4 h2-bold">Shopping Cart</h2>
+      {!cart || cart.items.length === 0 ? (
+        <div>
+          Cart is Empty <Link href={"/"}>Go Shopping</Link>
+        </div>
+      ) : (
+        <div className="grid md:grid-cols-4 md:gap-5">
+          <div className="overflow-x-auto md:col-span-3">Table</div>
+        </div>
+      )}
+    </>
+  );
 }
--- ngnexusstore/app/(root)/cart/page.tsx
+++ ngnexusstore/app/(root)/cart/page.tsx
@@ -1,15 +1,17 @@
 import { Metadata } from "next";
 import React from "react";
 import CartTable from "./cart-table";
+import { getMyCart } from "@/lib/actions/cart.action";
 
 export const metadata: Metadata = {
   title: "Shopping Cart",
 };
 
-export default function Cart() {
+export default async function Cart() {
+  const cart = await getMyCart();
   return (
     <>
-      <CartTable />
+      <CartTable cart={cart} />
     </>
   );
 }
